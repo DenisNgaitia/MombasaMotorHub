fbe1d460cab1b9f02bc0275cdc40e61ba6e5a5cb
DenisNgaitia <123322871+DenisNgaitia@users.noreply.github.com>
2024-06-11 11:42:05 +0300

    Update component.jsx

--- version1/src/component.jsx
+++ version1/src/component.jsx
@@ -1,8 +1,3 @@
-/**
- * v0 by Vercel.
- * @see https://v0.dev/t/g5sdh4IIBGl
- * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
- */
 import Link from "next/link"
 
 export default function Component() {
