cfbf39538c9211c8a35485a17b81201170c64b30
DenisNgaitia <123322871+DenisNgaitia@users.noreply.github.com>
2024-06-11 11:42:33 +0300

    Update layout.jsx

--- version1/src/layout.jsx
+++ version1/src/layout.jsx
@@ -1,6 +1,3 @@
-// This is the root layout component for your Next.js app.
-// Learn more: https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#root-layout-required
-
 import { Gabarito } from 'next/font/google'
 import { Comfortaa } from 'next/font/google'
 import './styles.css'
